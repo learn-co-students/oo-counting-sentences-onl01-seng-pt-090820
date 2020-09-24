3237ed8d796d9447c1ec241d6869689df25766ff
DavidRMorphew <morphewd@umich.edu>
2020-09-24 12:28:04 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,19 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-     self[-1] == "."
+    self.end_with?(".")
+    # self[-1] == "."
     # self.split.last[-1] == "."
   end
 
   def question?
-
+    self.end_with?("?")
+    # self[-1] == "?"
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.gsub(",","").gsub("? ", ". ").gsub("! ", ". ").split(". ").count
   end
 end
