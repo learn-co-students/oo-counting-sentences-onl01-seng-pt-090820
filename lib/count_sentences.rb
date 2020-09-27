917c9754851f4889fe7c7fe258d437f2c24518aa
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-27 08:48:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
+self.split(/[.!?]/).reject {|x| x.empty?}.size 
+    # returns the number of sentences in a string
+    # returns zero if there are no sentences in a string
+    # returns the number of sentences in a complex string
   end
 end
