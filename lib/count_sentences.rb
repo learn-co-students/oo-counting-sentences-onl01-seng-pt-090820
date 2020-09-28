9c316d129c88a72d8a5986c476d3f67d204ef6c0
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-28 01:59:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    #split based on RegEx params for ?, !, or .
+    self.split(/[.?!]/).reject { |x| x.empty? }.length
+      #when you use reject, you're giving it the params of what to TAKE OUT.  In this case, take out empty strings.  then use length to return the string count
+      
   end
 end
